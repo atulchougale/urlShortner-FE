4a760da5e7271ef404fa939ec4ccbdf7853db8bb
atul chougale <atulchougale96@gmail.com>
2023-06-05 17:48:17 +0530

    l

--- src/pages/private/components/TableCard.js
+++ src/pages/private/components/TableCard.js
@@ -22,7 +22,7 @@ const TableCard = ({ urlData }) => {
                     <th scope="row">{index + 1}</th>
                     <td>{item.originalUrl}</td>
                     <td>
-                   <a href={`https://urlshortner-rum2.onrender.com/{item.shortUrlId}`} target="_blank" rel="noreferrer">https://urlshortner-rum2.onrender.com/{item.shortUrlId}</a>
+                   <a href={`{item.shortUrlId}`} target="_blank" rel="noreferrer">https://urlshortner-rum2.onrender.com/{item.shortUrlId}</a>
                     </td>
                     <td>{new Date(item.createdAt).toLocaleDateString()}</td>
                     <td
